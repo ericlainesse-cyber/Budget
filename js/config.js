11a7eeb00c3f740438dd0b9ab4504ba09de69b18
ericlainesse-cyber <ericlainesse@gmail.com>
2026-06-12 22:37:49 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
 
-const API_URL='PASTE_APPS_SCRIPT_WEBAPP_URL_HERE';
+const API_URL='https://script.google.com/macros/s/AKfycbx43YeW9q4DRLtOCvV2c0Bpa8b8UYFHM2siIBglz0qvBRKHU7birpARnNiqcoswpz0Qvw/exec';
